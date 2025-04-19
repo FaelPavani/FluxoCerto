e2b8ab5214614ef7e460fb860c65edaa38dcd208
guireboko <guilherme.rferreira@sptech.school>
2025-04-19 14:23:57 -0300

    java Ec2

--- java/conexao-banco/src/main/java/school/sptech/Workbook.java
+++ java/conexao-banco/src/main/java/school/sptech/Workbook.java
@@ -1,25 +1,82 @@
 package school.sptech;
 
 import school.sptech.apachePOI.LeitorExcel;
+import school.sptech.bucket.S3Provider;
+import software.amazon.awssdk.core.sync.ResponseTransformer;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.ListObjectsRequest;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+import software.amazon.awssdk.services.s3.model.S3Object;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 
 public class Workbook{
     public static void main(String[] args) throws IOException {
+
+        S3Client s3Client = new S3Provider().getS3Client();
+        String bucketName = "s3-guilhermerebou";
+
         // String nomeArquivo = "curated-demanda-de-passageiros-por-estacao-2020-2024.xlsx";
-        String nomeArquivo = "curated-entrada-passageiros-por-linha-2020-2024.xlsx";
-        // Coloque o caminho para a pasta que estão os arquivos
-        Path caminho = Path.of("" + nomeArquivo);
-        InputStream arquivo = Files.newInputStream(caminho);
+        List<String> arquivos = Arrays.asList("curated-demanda-de-passageiros-por-estacao-2020-2024.xlsx", "curated-entrada-passageiros-por-linha-2020-2024.xlsx");
 
-        LeitorExcel leitor = new LeitorExcel();
-        leitor.extrairDados(nomeArquivo, arquivo);
+        for (int i = 0; i < arquivos.size(); i++) {
+            String nomeArquivo = arquivos.get(i);
 
-        // Para quando parar de ler
-        arquivo.close();
+            //Apagando o arquivo existente do bucket para atualiza-lo
+            // Caminho do arquivo que você quer excluir
+            Path caminhoGet = Path.of(nomeArquivo);
+
+            if (Files.exists(caminhoGet)) {
+                System.out.println("Passou");
+                 try {
+                     Files.delete(caminhoGet); // Deleta o arquivo
+                     System.out.println("Arquivo deletado com sucesso!");
+                 } catch (IOException e) {
+                     System.out.println("Erro ao deletar o arquivo: " + e.getMessage());
+                 }
+            } else {
+                System.out.println("Não passou");
+            }
+        }
+
+        //Fazendo download dos arquivos do Bucket
+        try {
+            List<S3Object> objects = s3Client.listObjects(ListObjectsRequest.builder().bucket(bucketName).build()).contents();
+            for (S3Object object : objects) {
+                GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                        .bucket(bucketName)
+                        .key(object.key())
+                        .build();
+
+                InputStream inputStream = s3Client.getObject(getObjectRequest, ResponseTransformer.toInputStream());
+                Files.copy(inputStream, new File(object.key()).toPath());
+                System.out.println("Arquivo baixado: " + object.key());
+            }
+        } catch (IOException | S3Exception e) {
+            System.err.println("Erro ao fazer download dos arquivos: " + e.getMessage());
+        }
+
+        for (int i = 0; i < arquivos.size(); i++) {
+            String nomeArquivo = arquivos.get(i);
+            // Coloque o caminho para a pasta que estão os arquivos
+            Path caminho = Path.of(nomeArquivo);
+
+            InputStream arquivo = Files.newInputStream(caminho);
+
+            LeitorExcel leitor = new LeitorExcel();
+            leitor.extrairDados(nomeArquivo, arquivo);
+
+            // Para quando parar de ler
+            arquivo.close();
+        }
 
         // Esse arquivo faz a conexão como banco de dados
         // Configure as variaveis de ambiente no IntelliJ
