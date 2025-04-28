49a2ea7c3968301588fa59e70c92e0c27cc544ff
Viviane dos Santos <viviane.santos@sptech.school>
2025-04-28 08:08:48 -0300

    Mudanças no EntradaPorLinhaDao.java

--- java/extracao-dados/src/main/java/school/sptech/conexaoBanco/dao/EntradaPorLinhaDao.java
+++ java/extracao-dados/src/main/java/school/sptech/conexaoBanco/dao/EntradaPorLinhaDao.java
@@ -44,7 +44,6 @@ public class EntradaPorLinhaDao {
         String sql = """
         INSERT INTO entradaPorLinha (id, fk_empresa, dataColeta, linha, fluxoTotal, mediaDia, maiorMaximaDiaria) 
         SELECT (?, 1, ?, ?, ?, ?, ?)
-        FROM DUAL
         WHERE NOT EXISTS ( SELECT 1 FROM entradaPorLinha WHERE id = ? )
         """;
         List<Object[]> batchArgs = new ArrayList<>();
