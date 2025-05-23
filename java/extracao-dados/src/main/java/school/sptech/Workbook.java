ee2f8991055b6b4a843d84169a2cb5e71581fad6
vivids12 <viviane.santos@sptech.school>
2025-05-23 19:22:23 -0300

    jar atualizado

--- java/extracao-dados/src/main/java/school/sptech/Workbook.java
+++ java/extracao-dados/src/main/java/school/sptech/Workbook.java
@@ -22,8 +22,10 @@ import java.util.List;
 public class Workbook{
     public static void main(String[] args) throws IOException, SQLException {
 
+        LeitorExcel.enviarMensagem("teste");
+
         S3Client s3Client = new S3Provider().getS3Client();
-        String bucketName = "dl-fluxo-certo";
+        String bucketName = "dl-fluxo-certo1";
 
         List<String> arquivos = Arrays.asList("curated-demanda-de-passageiros-por-estacao-2020-2024.xlsx", "curated-entrada-passageiros-por-linha-2020-2024.xlsx");
 
