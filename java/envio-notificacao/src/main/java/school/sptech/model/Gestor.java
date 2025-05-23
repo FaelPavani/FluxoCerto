59d31a624e9304bcb635f3484997d40504fbc1d9
vivids12 <viviane.santos@sptech.school>
2025-05-23 08:52:51 -0300

    Jar de extração mandando mensagem no slack

--- java/envio-notificacao/src/main/java/school/sptech/model/Gestor.java
+++
@@ -1,9 +0,0 @@
-package school.sptech.model;
-
-public class Gestor extends Funcionario{
-    public Gestor(Integer id, String nome, String cpf, String email, String telefone) {
-        super(id, nome, cpf, email, telefone);
-    }
-
-
-}
---
+++ java/extracao-dados/src/main/java/school/sptech/conexaoBanco/models/Funcionario.java
@@ -0,0 +1,80 @@
+package school.sptech.conexaoBanco.models;
+
+import java.sql.Date;
+
+public abstract class Funcionario {
+    private Integer id;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String telefone;
+    private Date dtNasc;
+
+    public Funcionario(Integer id, String nome, String cpf, String email, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Date getDtNasc() {
+        return dtNasc;
+    }
+
+    public void setDtNasc(Date dtNasc) {
+        this.dtNasc = dtNasc;
+    }
+
+    @Override
+    public String toString() {
+        return "Funcionario{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", email='" + email + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", dtNasc=" + dtNasc +
+                '}';
+    }
+}
